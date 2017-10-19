972ef61b5d3abe801a75cdd3a1f0810a09564d1d
Megane <megane.pau@edu.ece.fr>
2017-10-20 00:26:51 +0200

    Genre.java

--- src/tests/GenreTest.java
+++ src/tests/GenreTest.java
@@ -11,23 +11,29 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import mediatheque.Genre;
+
 /**
  * @author Megou
  *
  */
 public class GenreTest {
 
-	
+	Genre genre1;
 	/**
 	 * @throws java.lang.Exception
 	 */
 	@Before
 	public void setUp() throws Exception {
+		genre1 = new Genre("fantasy");
 	}
 	
 	@Test
 	public void emprunterTest() {
-		fail("Not yet implemented");
+		int expected;
+		expected = genre1.getNbEmprunts() + 1;
+		genre1.emprunter();
+		assertEquals(expected, genre1.getNbEmprunts());
 	}
 
 }
