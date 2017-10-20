08906e35fe632aaed0ba689394a2862d49f51de9
Megane <megane.pau@edu.ece.fr>
2017-10-20 14:47:09 +0200

    Video test constructeur (erreur trouvée)

--- src/tests/VideoTest.java
+++ src/tests/VideoTest.java
@@ -25,6 +25,54 @@ public class VideoTest {
 		video = new Video("aaa", localisation, "titre1", "auteur1", "2002", genre, 200, "mention légale");
 	}
 
+	
+	@Test
+	public void constructeurVideoTest() throws OperationImpossible, InvariantBroken{
+		Localisation loca = new Localisation("salle1", "rayon 1");
+		Genre genr = new Genre("genre1");
+		Video video2 = new Video("aaa", loca, "titre1", "auteur1", "2002", genr, 200, "mention");
+		assertEquals("Le code n'est pas introduit dans le document. ","aaa",video2.getCode());
+		assertEquals("La localisation n'est pas introduite dans le document.", loca, video2.getLocalisation());
+		assertEquals("Le titre n'est pas introduit dans le document.", "titre1", video2.getTitre());
+		assertEquals("L'auteur n'est pas introduit dans le document. ", "auteur1", video2.getAuteur());
+		assertEquals("L'année n'est pas introduite dans le document. ", "2002", video2.getAnnee());
+		assertEquals("Le genre n'est pas introduit dans le document", genr, video2.getGenre());
+		assertFalse("Le document est initialisé en tant qu'empruntable", video2.estEmpruntable());
+		assertFalse("Le document est initialisé en tant qu'emprunté.", video2.estEmprunte());
+		assertEquals("Le nombre d'emprunts n'est pas initialisé à 0", 0, video2.getNbEmprunts());
+		assertEquals("La durée n'est pas correctement introduite.", 200, video2.getDureeFilm());
+		assertEquals("La mention légale n'est pas correctement introduite.", "mention", video2.getMentionLegale());
+	}
+	
+	@Test(expected = OperationImpossible.class)
+	public void constructeurVideoDureeNegTest() throws OperationImpossible, InvariantBroken{
+		Localisation loca = new Localisation("salle1", "rayon 1");
+		Genre genr = new Genre("genre1");
+		Video video2 = new Video("aaa", loca, "titre1", "auteur1", "2002", genr, -10, "mention");
+	}
+	
+	@Test(expected = OperationImpossible.class)
+	public void constructeurVideoMentionNullTest() throws OperationImpossible, InvariantBroken{
+		Localisation loca = new Localisation("salle1", "rayon 1");
+		Genre genr = new Genre("genre1");
+		Video video2 = new Video("aaa", loca, "titre1", "auteur1", "2002", genr, 200, null);
+	}
+	
+	@Test(expected = OperationImpossible.class)
+	public void constructeurVideoDocNullTest() throws OperationImpossible, InvariantBroken{
+		Localisation loca = new Localisation("salle1", "rayon 1");
+		Genre genr = null;
+		Video video2 = new Video("aaa", loca, "titre1", "auteur1", "2002", genr, 200, "mention");
+	}
+	
+	@Test(expected = OperationImpossible.class)
+	public void constructeurVideoDocNullDureeNegTest() throws OperationImpossible, InvariantBroken{
+		Localisation loca = new Localisation("salle1", "rayon 1");
+		Genre genr = new Genre("genre1");
+		Video video2 = new Video("aaa", loca, null, "auteur1", "2002", genr, -10, "mention");
+	}
+	
+	
 
 	@Test
 	public void emprunterTest1() throws InvariantBroken, OperationImpossible {
